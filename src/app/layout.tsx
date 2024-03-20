3441ac8a87fcea1245b786b12a5831c102a447d3
Player-lnwza007 <wptrainict02@gmail.com>
2024-03-20 15:48:27 +0700

    temple update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          h
+        </div>
+        {children}
+        <div>
+          f
+        </div>
+      </body>
     </html>
   );
 }
